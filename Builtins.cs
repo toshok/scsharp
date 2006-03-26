54ad1d9064de06bc5495b2ed6884405fc93f096b
toshok <toshok@ea3e6beb-b7ab-da11-bdaa-00e0814018bc>
2006-03-26 16:38:00 +0000

    sync
    git-svn-id: svn+ssh://scsharp.hungry.com/svn/trunk/starcraft-sharp@182 ea3e6beb-b7ab-da11-bdaa-00e0814018bc

--- Builtins.cs
+++ Builtins.cs
@@ -21,5 +21,33 @@ namespace Starcraft {
 		public static string rez_GluScoreBin = "rez\\gluScore.bin";
 
 		public static string Scorev_pMainPcx = "glue\\score{0}v\\pMain.pcx";
+
+		/* scripts */
+		public static string IScriptBin = "scripts\\iscript.bin";
+		public static string AIScriptBin = "scripts\\aiscript.bin";
+
+		/* arr files */
+		public static string FlingyDat = "arr\\flingy.dat";
+		public static string FlingyTbl = "arr\\flingy.tbl";
+		public static string ImagesDat = "arr\\images.dat";
+		public static string ImagesTbl = "arr\\images.tbl";
+		public static string MapdataDat = "arr\\mapdata.dat";
+		public static string MapdataTbl = "arr\\mapdata.tbl";
+		public static string OrdersDat = "arr\\orders.dat";
+		public static string OrdersTbl = "arr\\orders.tbl";
+		public static string PortdataDat = "arr\\portdata.dat";
+		public static string PortdataTbl = "arr\\portdata.tbl";
+		public static string SfxdataDat = "arr\\sfxdata.dat";
+		public static string SfxdataTbl = "arr\\sfxdata.tbl";
+		public static string SpritesDat = "arr\\sprites.dat";
+		public static string SpritesTbl = "arr\\sprites.tbl";
+		public static string TechdataDat = "arr\\techdata.dat";
+		public static string TechdataTbl = "arr\\techdata.tbl";
+		public static string UnitsDat = "arr\\units.dat";
+		public static string UnitsTbl = "arr\\units.tbl";
+		public static string UpgradesDat = "arr\\upgrades.dat";
+		public static string UpgradesTbl = "arr\\upgrades.tbl";
+		public static string WeaponsDat = "arr\\weapons.dat";
+		public static string WeaponsTbl = "arr\\weapons.tbl";
 	}
 }
