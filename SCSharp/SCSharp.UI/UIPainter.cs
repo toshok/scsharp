b5c78dfbab42f6b56eeb0ac60b8abde0239c92fd
toshok <toshok@ea3e6beb-b7ab-da11-bdaa-00e0814018bc>
2006-04-26 02:28:18 +0000

    add license foo
    git-svn-id: svn+ssh://scsharp.hungry.com/svn/trunk/starcraft-sharp@243 ea3e6beb-b7ab-da11-bdaa-00e0814018bc

--- SCSharp/SCSharp.UI/UIPainter.cs
+++ SCSharp/SCSharp.UI/UIPainter.cs
@@ -1,6 +1,35 @@
-//#define SHOW_ELEMENT_BORDERS
+//
+// SCSharp.UI.UIPainter
+//
+// Authors:
+//	Chris Toshok (toshok@hungry.com)
+//
+// (C) 2006 The Hungry Programmers (http://www.hungry.com/)
+//
+
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.IO;
 
@@ -9,9 +38,20 @@ using System.Drawing;
 
 namespace SCSharp.UI
 {
-
 	public class UIPainter
 	{
+		static bool showBorders;
+
+		static UIPainter()
+		{
+#if DEBUG
+			string sb = ConfigurationManager.AppSettings ["showElementBorders"];
+			if (sb != null) {
+				showBorders = Boolean.Parse (sb);
+			}
+#endif
+		}
+
 		List<UIElement> elements;
 
 		public UIPainter (List<UIElement> elements)
@@ -25,11 +65,12 @@ namespace SCSharp.UI
 				UIElement e = elements[i];
 
 				e.Paint (surf, now);
-#if SHOW_ELEMENT_BORDERS
-				surf.DrawBox (new Rectangle (new Point (e.X1,e.Y1), new Size (e.Width - 1, e.Height - 1)), e.Visible ? Color.Green : Color.Yellow);
-				if (e.Text == "")
-					e.Text = i.ToString();
-#endif
+
+				if (showBorders) {
+					surf.DrawBox (new Rectangle (new Point (e.X1,e.Y1), new Size (e.Width - 1, e.Height - 1)), e.Visible ? Color.Green : Color.Yellow);
+					if (e.Text == "")
+						e.Text = i.ToString();
+				}
 			}
 		}
 	}
