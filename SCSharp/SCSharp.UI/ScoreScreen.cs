3865f47fdcd2a74d2f03435a286161654c29a9b2
toshok <toshok@ea3e6beb-b7ab-da11-bdaa-00e0814018bc>
2010-08-25 20:36:30 +0000

    sync
    git-svn-id: svn+ssh://scsharp.hungry.com/svn/scsharp/trunk@2378 ea3e6beb-b7ab-da11-bdaa-00e0814018bc

--- SCSharp/SCSharp.UI/ScoreScreen.cs
+++ SCSharp/SCSharp.UI/ScoreScreen.cs
@@ -32,28 +32,23 @@ using System;
 using System.IO;
 using System.Threading;
 
-using Gtk;
-using Gdk;
-
 namespace SCSharp.UI
 {
 	public class ScoreScreen : UIScreen
 	{
+#if false
 		SwooshPainter swoosher;
-		Painter painter;
-		CursorAnimator cursor;
-		Gdk.Pixbuf background;
-		Bin ui;
-		Mpq mpq;
-		bool translucent;
+#endif
+		bool victorious;
 
-		Gdk.Pixbuf pMainPb;
-
-		public ScoreScreen (Mpq mpq)
+		public ScoreScreen (Mpq mpq, bool victorious)
+			: base  (mpq, String.Format ("glue\\Pal{0}{1}", Util.RaceChar[(int)Game.Instance.Race], victorious ? 'v' : 'd'), Builtins.rez_GluScoreBin)
 		{
 			this.mpq = mpq;
+			this.victorious = victorious;
 		}
 
+#if false
 		void DoneSwooshingIn ()
 		{
 			Console.WriteLine ("Done Swooshing In");
@@ -85,56 +80,16 @@ namespace SCSharp.UI
 		{
 			base.SwooshOut();
 		}
+#endif
 
-		public override void AddToPainter (Painter painter)
+		const int MAIN_IMAGE_INDEX = 1;
+		protected override void ResourceLoader ()
 		{
-			this.painter = painter;
-			painter.Add (Layer.Background, BackgroundPainter);
-		}
+			base.ResourceLoader ();
 
-		public override void RemoveFromPainter (Painter painter)
-		{
-			base.RemoveFromPainter (painter);
-		}
-
-		protected override void ResourceLoader (object state)
-		{
-			Console.WriteLine ("loading arrow cursor");
-			cursor = new CursorAnimator ((Grp)mpq.GetResource (String.Format (Builtins.Palv_ArrowGrp, Util.RaceChar[(int)Game.Instance.Race])));
-			cursor.SetHotSpot (64, 64);
-			Console.WriteLine ("loading score background");
-			background = new Gdk.Pixbuf ((Stream)mpq.GetResource (String.Format (Builtins.Palv_BackgroundPcx, Util.RaceChar[(int)Game.Instance.Race])));
-			Console.WriteLine ("loading score screen ui elements");
-			ui = (Bin)mpq.GetResource (Builtins.rez_GluScoreBin);
-
-			if (ui.Elements[1].type != UIElementType.Image)
-				throw new Exception ();
-			ui.Elements[1].text = String.Format (Builtins.Scorev_pMainPcx, Util.RaceChar[(int)Game.Instance.Race]);
-
-			/* resolve external entities */
-			foreach (UIElement e in ui.Elements) {
-				if (e.type == UIElementType.Image) {
-					//XXX
-					if (e.text != null && e.text != "") {
-						Console.WriteLine ("loading image {0}", e.text);
-						e.resolvedData = new Surface (Bitmap.FromStream ((Stream)mpq.GetResource (e.text)));
-					}
-				}
-			}
-
-			ui.Elements[1].DumpFlags();
-
-			if ((ui.Elements[1].flags & UIElementFlags.ApplyTranslucency) == UIElementFlags.ApplyTranslucency)
-				translucent = true;
-			pMainPb = (Gdk.Pixbuf)ui.Elements[1].resolvedData;
-		}
-
-		protected override void FinishedLoading ()
-		{
-			Background = background;
-			UIPainter = new UIPainter (ui);
-
-			base.FinishedLoading ();
+			Elements[MAIN_IMAGE_INDEX].Text = String.Format (victorious ? Builtins.Scorev_pMainPcx : Builtins.Scored_pMainPcx,
+									 Util.RaceChar[(int)Game.Instance.Race]);
+			Console.WriteLine ("screen path = {0}", Elements[MAIN_IMAGE_INDEX].Text);
 		}
 	}
 }
